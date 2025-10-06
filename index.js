e7b555d47d8c919ffe1fc9ac9132bc48e27ec9c7
SolarDaz3 <jeremygalan@hotmail.com>
2025-10-06 17:47:59 -0400

    api functionality

--- index.js
+++ index.js
@@ -7,3 +7,44 @@ function hideSidebar(){
    const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none';
 }
+
+
+
+async function fetchData() {
+
+    try{
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch( `https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+
+        if(!response.ok){
+            throw new Error("Pokemon not found");
+        }
+
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = 'block';
+
+
+    }
+    catch(error){
+        console.error(error);
+    }
+    
+}
+
+
+
+
+
+// fetch("https://pokeapi.co/api/v2/pokemon/infernape")
+//     .then(response => {
+//         if(!response.ok){
+//             throw new Error("Pokemon not found");
+//         }
+//         return response.json();
+//     })
+//     .then(data => console.log(data.name))
+//     .catch(error => console.error(error));
